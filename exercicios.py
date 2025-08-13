259cc479e71b91e189f16ae3d1ef51ea85f16fc1
Dantas <gr.rosatto@gmail.com>
2025-08-12 21:00:02 -0300

    Adicionando novos exercicios resolvidos

--- exercicios.py
+++ exercicios.py
@@ -69,10 +69,31 @@ print(resultado_fahrenheit)
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+palavra = input('Digite uma palavra: ')
+palavra_maiuscula = palavra.upper()
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+nome = input('Digite seu nome completo: ')
+nome_minusculo = nome.lower()
+print(nome_minusculo)
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+data = input('Digite uma data no formato dd/mm/aaaa : ')
+lista_data = data.split("/")
+dia = lista_data[0]
+mes = lista_data[1]
+ano = lista_data[2]
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+palavra1 = input('Digite a primeira palavra: ')
+palavra2 = input('Digite a segunda palavra: ')
+palavra_concatenada = palavra1 + palavra2
+print(palavra_concatenada)
 
 # #### Booleanos (`bool`)
 
