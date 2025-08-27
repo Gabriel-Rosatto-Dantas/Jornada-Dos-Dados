50630e6841a7e68c6ed4e598497ace5716b06379
Gabriel Rosatto Dantas <105675641+Gabriel-Rosatto-Dantas@users.noreply.github.com>
2025-08-27 13:35:21 -0300

    Add files via upload

--- Aula03/desafio.py
+++ Aula03/desafio.py
@@ -3,44 +3,50 @@
 # informações corretas
 
 # Solicita ao usuário que digite seu nome
+
 nome_valido = False
 salario_valido = False
 bonus_valido = False
 
-while not nome_valido:
+while nome_valido is not True:
     try:
-        nome = input("Digite seu nome: ")
+        nome = "Luciano"
 
         # Verifica se o nome está vazio
         if len(nome) == 0:
             raise ValueError("O nome não pode estar vazio.")
         # Verifica se há números no nome
-        elif any(char.isdigit() for char in nome):
-            raise ValueError("O nome não deve conter números.")
+        elif nome.isdigit():
+            raise ValueError("O nome não deve conter números.") 
         else:
-            print("Nome válido:", nome)
             nome_valido = True
+            print("Nome válido:", nome)
     except ValueError as e:
         print(e)
+# Solicita ao usuário 
+# que digite o valor do seu salário e converte para float
 
-# Solicita ao usuário que digite o valor do seu salário e converte para float
-
-try:
-    salario = float(input("Digite o valor do seu salário: "))
-    if salario < 0:
-        print("Por favor, digite um valor positivo para o salário.")
-except ValueError:
-    print("Entrada inválida para o salário. Por favor, digite um número.")
-    exit()
+while salario_valido is not True:
+    try:
+        salario = 2000
+        if salario < 0:
+            print("Por favor, digite um valor positivo para o salário.")
+        else:
+            salario_valido = True
+    except ValueError:
+        print("Entrada inválida para o salário. Por favor, digite um número.")
 
 # Solicita ao usuário que digite o valor do bônus recebido e converte para float
-try:
-    bonus = float(input("Digite o valor do bônus recebido: "))
-    if bonus < 0:
-        print("Por favor, digite um valor positivo para o bônus.")
-except ValueError:
-    print("Entrada inválida para o bônus. Por favor, digite um número.")
-    exit()
+        
+while bonus_valido is not True:        
+    try:
+        bonus = 3.0
+        if bonus < 0:
+            print("Por favor, digite um valor positivo para o bônus.")
+        else:
+            bonus_valido = True
+    except ValueError:
+        print("Entrada inválida para o bônus. Por favor, digite um número.")
 
 bonus_recebido = 1000 + salario * bonus  # Exemplo simples de KPI
 
