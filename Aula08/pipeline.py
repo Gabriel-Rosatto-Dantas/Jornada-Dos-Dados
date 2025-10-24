c93bc41f529c33e75404f3bc9300117eb3563770
Dantas <gr.rosatto@gmail.com>
2025-10-23 22:10:13 -0300

    adicionando pipeline.py

--- Aula08/pipeline.py
+++ Aula08/pipeline.py
@@ -1,2 +1,6 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+from etl import pipeline_calcular_kpi_de_vendas_consolidado
+
+pasta_argumento: str = 'data'
+formato_de_saida: list = ["csv", "parquet"]
+
+pipeline_calcular_kpi_de_vendas_consolidado(pasta_argumento, formato_de_saida)
